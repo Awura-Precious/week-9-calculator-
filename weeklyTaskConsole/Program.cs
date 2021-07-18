a6e0f374d6b21dcd844e8645525ed6207fe2246a
Awura-Precious <57904360+Awura-Precious@users.noreply.github.com>
2021-07-18 15:22:13 -0700

    performing operations on user input

---
+++ weeklyTaskConsole/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using weeklyTaskLib;
+
+
+namespace weeklyTaskConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Input Two numbers and operator:");
+            System.Console.WriteLine("Operation types: +, -, *,/,^");
+
+
+
+
+            try
+            {
+                //instantiating Calculator class 
+                Calculator values =new Calculator
+                {
+                    FirstNumber=System.Convert.ToDouble(System.Console.ReadLine()),
+                    SecondNumber = System.Convert.ToDouble(System.Console.ReadLine()),
+                    
+                    Operation=System.Console.ReadLine(),
+                };
+                
+                //saving data in a list 
+                // List<Calculator> history = new List<Calculator>();
+                // history.Add(values);
+
+                // foreach (var data in history)
+                // {
+                //     Console.WriteLine(data);
+                // }
+
+
+                //checking for the type of operator
+                if (values.Operation =="+")
+                {
+                    System.Console.WriteLine(values.Sum(values.FirstNumber,values.SecondNumber));
+                }
+
+                else if (values.Operation =="*")
+                {
+                    System.Console.WriteLine(values.Product(values.FirstNumber,values.SecondNumber));
+                }
+                else if (values.Operation =="/")
+                {
+                    System.Console.WriteLine(values.Division(values.FirstNumber,values.SecondNumber));
+                }
+                else if (values.Operation =="^")
+                {
+                    System.Console.WriteLine(values.Square(values.FirstNumber));
+                }
+                else if (values.Operation =="-")
+                {
+                    System.Console.WriteLine(values.Subtraction(values.FirstNumber,values.SecondNumber));
+                }
+
+
+
+                else
+                {
+                    Console.WriteLine("please enter the right Operation eg: ^ ");
+                }
+            }
+            catch (FormatException)
+            {
+                
+                Console.WriteLine("Please input a value ");
+            }
+
+            catch (Exception)
+            {
+                
+                Console.WriteLine("follows all instructions  ");
+            }
+
+
+        }
+    }
+}
---
+++ weeklyTaskLib/Calculator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace weeklyTaskLib
+{
+
+    public  class Calculator 
+    {
+
+
+        //encapsulating user inputs and operator 
+        private double _numOne;
+        private double _numTwo;
+        private string _op;
+
+        
+
+        public double FirstNumber
+        { 
+            get 
+                {return _numOne;}
+            set
+                {_numOne=value;} 
+            
+        }
+
+         public double SecondNumber
+        { 
+            get 
+                {return _numTwo;}
+            set
+                {_numTwo=value;} 
+            
+        }
+
+        public string Operation 
+        {
+            get 
+                {return _op;}
+            set
+                {_op=value;} 
+            
+
+        }
+
+
+        //methods for operations 
+        public double Sum(double first, double sec)
+        {
+            try
+            {
+                double  result = first + sec;
+                return result;
+                
+            }
+            catch (Exception)
+            {
+                
+                return 0;
+            }
+        }
+
+        public double Product(double first, double sec)
+        {
+            try
+            {
+                double result = first * sec;
+                return result;
+                
+            }
+            catch (Exception)
+            {
+                
+                return 0;
+            }
+        }
+
+        public double Division(double first, double sec)
+        {
+            try
+            {
+                double  result = first / sec;
+                return result;
+                
+            }
+            catch (Exception)
+            {
+                
+                return 0;
+            }
+        }
+
+        public double Subtraction(double first, double sec )
+        {
+            try
+            {
+                double  result = first +  sec;
+                return result;
+                
+            }
+            catch (Exception)
+            {
+                
+                return 0;
+            }
+        }
+
+        public double Square(double first)
+        {
+            try
+            {
+                double  result = Math.Pow(first,2);
+                return result;
+                
+            }
+            catch (Exception)
+            {
+                
+                return 0;
+            }
+        }
+
+        // public override string ToString()
+        // {
+        //     return $"the result of {FirstNumber} and {SecondNumber} ";
+        // }
+        
+
+       
+
+    }
+}
